d7737d77b140ab7bb94917ef619785b9ff9a77c9
Dong Kyu Lee <stvboy00@gmail.com>
2023-04-07 21:41:04 +0900

    Mouse click can detect the green boxes

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonUp(0))
+        {
+            Vector3 MousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.DrawRay(MousePosition, Vector3.forward * 10.0f, Color.red, 3.0f);
+
+            RaycastHit2D hit = Physics2D.Raycast(MousePosition, Vector3.forward, 10.0f);
+            if (hit)
+            {
+                hit.transform.gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+            }
+        }
+    }
+}
--- Assets/Scripts/UI_EventHandler.cs
+++ Assets/Scripts/UI_EventHandler.cs
@@ -5,24 +5,13 @@ using UnityEngine.EventSystems;
 
 public class UI_EventHandler : MonoBehaviour, IDragHandler, IPointerClickHandler
 {
-    private float timeCount;
-
     public void OnDrag(PointerEventData data)
     {
         gameObject.transform.position = data.position;
-        if (data.dragging)
-        {
-            timeCount += Time.deltaTime;
-            if (timeCount > 1.0f)
-            {
-                Debug.Log("Dragging:" + data.position);
-                timeCount = 0.0f;
-            }
-        }
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        Debug.Log(name + " Game Object Clicked!");
+        Debug.Log($"current position is {Camera.main.ScreenToWorldPoint(eventData.position)}");
     }
 }
