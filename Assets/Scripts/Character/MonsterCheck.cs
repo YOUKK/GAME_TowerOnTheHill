0d618d8b2dc276ffe7631b984f335de748ba93c9
Djklsfj_Ryul <102909120+Djklsfj-Ryul@users.noreply.github.com>
2023-06-06 17:52:39 +0900

    ErrorFix

---
+++ Assets/Scripts/Character/MonsterCheck.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterCheck : MonoBehaviour
+{
+    private Character mainCharacter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCharacter = transform.GetComponentInParent<Character>();
+        transform.position = new Vector2(transform.position.x + ((1 + mainCharacter.Range) / 2),
+                                         transform.position.y);
+        transform.localScale = new Vector2(mainCharacter.Range, transform.localScale.y);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            mainCharacter.CheckMonster = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            mainCharacter.CheckMonster = false;
+        }
+    }
+}
--- Assets/Scripts/Character/Resource & Defense/SunFlower.cs
+++ Assets/Scripts/Character/Resource & Defense/SunFlower.cs
@@ -8,18 +8,6 @@ public class SunFlower : Character
     protected override void Start()
     {
         base.Start();
-        //소환 쿨타임 2초
-        CoolTime = 2f;
-        //공격력 1 (변함 없음)
-        //체력 2
-        HealthPoint = 2f;
-        //투사체 속도 1 (변함 없음)
-        //공격 속도 5
-        AttackDelay = 5f;
-        //투사체 개수 1 (변함 없음)
-        //사거리 1 (변함 없음)
-        //공격(자원)유지 시간 7초
-        AttackDuration = 15.0f;
     }
 
     public override void Attack()
