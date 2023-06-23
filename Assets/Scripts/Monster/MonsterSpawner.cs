3ef4b2e61488278992bafae2eb2cfcc93f36c5cb
Dong Kyu Lee <stvboy00@gmail.com>
2023-06-23 16:16:36 +0900

    스폰된 몬스터 관리를 위한 Linked List 생성

--- Assets/Scripts/Monster/MonsterSpawner.cs
+++ Assets/Scripts/Monster/MonsterSpawner.cs
@@ -8,10 +8,12 @@ public class MonsterSpawner : MonoBehaviour
     public int          phase = 1;
     public int          stage = 1;
     public GameObject[] lines = new GameObject[5];
-    
+
     private int           count = 0;
     private int           idx = 0;
     private MonsterWave[] currentWave = null;
+
+    public LinkedList<GameObject>[] monstersInLine = new LinkedList<GameObject>[5];
 
     void Start()
     {
@@ -31,9 +33,14 @@ public class MonsterSpawner : MonoBehaviour
 
         if (currentWave[idx].time < Managers.TimeM.Sec)
         {
-            Instantiate(currentWave[idx].monsterInfo, 
+            GameObject obj = Instantiate(currentWave[idx].monsterInfo, 
                 lines[currentWave[idx].line].transform.position, transform.rotation);
-            
+
+            // 몬스터 생성 시, 생성될 라인의 Linked List의 뒤에 삽입.
+            // 몬스터 사망 시, 해당 라인의 Linked List에서 삭제할 필요 있음.
+            // 삭제는 MonsterSpawner에서 삭제하는 것이 좋을 듯. delegate로 삭제 함수를 호출하든,
+            // update에서 null된 몬스터를 찾아 삭제하든.
+            if (obj != null) monstersInLine[currentWave[idx].line].AddLast(obj); // 삽입
             ++idx;
         }
     }
