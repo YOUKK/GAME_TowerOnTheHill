cf9c53554ff917e39a2d13c4638694b1dd26afdc
Dong Kyu Lee <stvboy00@gmail.com>
2023-07-10 16:24:19 +0900

    (WIP)Aerial Monster animation adding

--- Assets/Scripts/Monster/AerialMonster.cs
+++ Assets/Scripts/Monster/AerialMonster.cs
@@ -4,30 +4,9 @@ using UnityEngine;
 
 public class AerialMonster : Monster
 {
-    void Update()
-    {
-        if (target == null) Move();
-        else
-        {
-            if (!isAttacking)
-            {
-                isAttacking = true;
-                StartCoroutine(AttackCoolCoroutine());
-            }
-        }
-        anim.SetBool("isAttack", isAttacking);
-    }
-
     protected override void Move()
     {
         base.Move();
-    }
-
-    protected override IEnumerator AttackCoolCoroutine()
-    {
-        Attack();
-        yield return new WaitForSeconds(status.hitSpeed);
-        isAttacking = false;
     }
 
     protected override void Attack()
