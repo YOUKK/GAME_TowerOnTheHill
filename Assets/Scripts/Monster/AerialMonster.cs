dc5582966ffaf9a27e6d1b083a700dcbba19c411
Dong Kyu Lee <stvboy00@gmail.com>
2023-07-11 16:03:08 +0900

    Aerial Monster animation added

--- Assets/Scripts/Monster/AerialMonster.cs
+++ Assets/Scripts/Monster/AerialMonster.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class AerialMonster : Monster
 {
+    protected override void Start()
+    {
+        base.Start();
+        transform.position = new Vector3(transform.position.x, transform.position.y + 0.4f);
+    }
+
     protected override void Move()
     {
         base.Move();
