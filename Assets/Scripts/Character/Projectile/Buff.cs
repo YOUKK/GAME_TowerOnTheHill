3724aae231747fe1284588f16a306751ac97b5b6
Djklsfj_Ryul <102909120+Djklsfj-Ryul@users.noreply.github.com>
2023-09-03 13:04:54 +0900

    Buffer

---
+++ Assets/Scripts/Character/Projectile/Buff.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buff : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch((int)gameObject.name[gameObject.name.Length - 1])
+        {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            default:
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
