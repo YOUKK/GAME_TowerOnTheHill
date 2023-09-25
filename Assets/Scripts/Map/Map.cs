885e51297269e6bd700357a98e8ad24d7f29152f
Dong Kyu Lee <stvboy00@gmail.com>
2023-09-25 16:24:36 +0900

    (WIP)Implementing function that change two random character position

--- Assets/Scripts/Map/Map.cs
+++ Assets/Scripts/Map/Map.cs
@@ -89,8 +89,9 @@ public class Map : MonoBehaviour
 
         seats[y][x].GetComponent<Seat>().character = Instantiate(character, seats[y][x].transform.position, transform.rotation);
         seats[y][x].GetComponent<Seat>().character.GetComponent<Character>().Location = new Vector2(x, y);
-        
-        if(character.GetComponent<Tower>() == null) // tower가 아닌 경우
+        seats[y][x].GetComponent<Seat>().character.name = character.name;
+
+        if (character.GetComponent<Tower>() == null) // tower가 아닌 경우
             seats[y][x].GetComponent<Seat>().isCharacterOn = true;
         seats[y][x].GetComponent<Seat>().usable = false;
         seats[y][x].GetComponent<Seat>().character.GetComponentInChildren<SpriteRenderer>().sortingOrder = y * (-1);
@@ -126,8 +127,21 @@ public class Map : MonoBehaviour
 
     public void ChangeCharacterSeat(Vector2 seat1, Vector2 seat2)
     {
-        // 구현해야 함
         // 다른 몬스터가 공격중인 캐릭터가 자리를 바꾸면 어떻게 해야할까
+        // 이미 존재하는 캐릭터의 위치를 바꾸지 말고, 존재하는 두 캐릭터를 죽이고
+        // 같은 종류의 새로운 캐릭터를 자리만 바꿔서 다시 생성한다.
+        // 이때, 원래 캐릭터의 체력 정보를 저장했다가 새로 생성된 애들한테 적용해줘야 함.
+        int x1 = (int)seat1.x;
+        int y1 = (int)seat1.y;
+        int x2 = (int)seat2.x;
+        int y2 = (int)seat2.y;
+
+        string characterName1 = seats[y1][x1].GetComponent<Seat>().character.name;
+        string characterName2 = seats[y2][x2].GetComponent<Seat>().character.name;
+        GameObject newCharacter1 = Resources.Load<GameObject>($"/Prefabs/Character/{characterName1}");
+        GameObject newCharacter2 = Resources.Load<GameObject>($"/Prefabs/Character/{characterName2}");
+
+
     }
 
     public Line GetLineInfo(int lineNum)
