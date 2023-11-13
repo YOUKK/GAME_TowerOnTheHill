10cb4b6ad50d9468731a60537190b289af417190
Dong Kyu Lee <stvboy00@gmail.com>
2023-11-13 13:20:21 +0900

    Added Boss monster class methods

--- Assets/Scripts/Monster/BossMonster.cs
+++ Assets/Scripts/Monster/BossMonster.cs
@@ -4,5 +4,77 @@ using UnityEngine;
 
 public class BossMonster : Monster
 {
+    public enum AttackPattern { Normal, First, Second, Third, }
+    
+    private AttackPattern pattern;
+    private GameObject firstAttackObj;
+    private GameObject secondAttackObj;
+    private GameObject thirdAttackObj;
+    private List<MonsterSpawnData> monsterSpawns;
 
+    private float patternDuration;
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        if(!isAttacking)
+        {
+            isAttacking = true;
+            switch (pattern)
+            {
+                case AttackPattern.Normal:
+                    {
+                        break;
+                    }
+                case AttackPattern.First:
+                    {
+                        break;
+                    }
+                case AttackPattern.Second:
+                    { 
+                        break; 
+                    }
+                case AttackPattern.Third:
+                    { 
+                        break; 
+                    }
+                default:
+                    break;
+            }
+        }
+    }
+
+    protected override void Move(float speed)
+    {
+        base.Move(speed);
+    }
+
+    private void ChangeAttackPattern()
+    {
+        
+    }
+
+    private IEnumerator NormalAttackCoroutine()
+    {
+        yield return null;
+    }
+
+    private IEnumerator FirstAttackCoroutine()
+    {
+        yield return null;
+    }
+
+    private IEnumerator SecondAttackCoroutine()
+    {
+        yield return null;
+    }
+
+    private IEnumerator ThirdAttackCoroutine()
+    {
+        yield return null;
+    }
 }
