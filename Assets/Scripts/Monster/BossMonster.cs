67be1ec93d7ec9f7db53bd02f9352a616a6343ee
Dong-Kyu-Lee <stvboy00@gmail.com>
2023-11-18 21:13:39 +0900

    (WIP)Boss monster pattern implementing)

--- Assets/Scripts/Monster/BossMonster.cs
+++ Assets/Scripts/Monster/BossMonster.cs
@@ -13,44 +13,28 @@ public class BossMonster : Monster
     private List<MonsterSpawnData> monsterSpawns;
 
     private float patternDuration;
-    private bool isPatternCool;
+    private bool isMove;
 
     protected override void Start()
     {
         base.Start();
         pattern = AttackPattern.Normal;
+        patternDuration = 5.0f;
+        isMove = false;
+        StartCoroutine(Think());
     }
 
     protected override void Update()
     {
-        if(!isPatternCool)
-        {
-            switch (pattern)
-            {
-                case AttackPattern.Normal:
-                    {
-                        break;
-                    }
-                case AttackPattern.First:
-                    {
-                        break;
-                    }
-                case AttackPattern.Second:
-                    { 
-                        break; 
-                    }
-                case AttackPattern.Third:
-                    { 
-                        break; 
-                    }
-                default:
-                    break;
-            }
-        }
-        else
+        if(isMove)
         {
             Move(currentSpeed);
         }
+    }
+
+    private void Idle()
+    {
+        // Idle animation;
     }
 
     protected override void Move(float speed)
@@ -62,43 +46,81 @@ public class BossMonster : Monster
     {
         if(currentHP >= (float)status.hp * 2/3)
         {
-
+            pattern = AttackPattern.First;
         }
         else if(currentHP >= (float)status.hp * 1/3)
         {
-
+            pattern = AttackPattern.Second;
         }
         else
         {
-
+            pattern = AttackPattern.Third;
         }
     }
 
-    private IEnumerator WalkCoroutine()
+    private IEnumerator Think()
     {
-        // Walk animation
-        isPatternCool = false;
+        ChangeAttackPattern();
+        // switch문으로 상태 결정
+        switch (pattern)
+        {
+            case AttackPattern.Normal:
+                {
+                    StartCoroutine(NormalAttackCoroutine());
+                    break;
+                }
+            case AttackPattern.First:
+                {
+                    StartCoroutine(FirstPatternCoroutine());
+                    break;
+                }
+            case AttackPattern.Second:
+                {
+                    StartCoroutine(SecondPatternCoroutine());
+                    break;
+                }
+            case AttackPattern.Third:
+                {
+                    StartCoroutine(ThirdPatternCoroutine());
+                    break;
+                }
+            default:
+                break;
+        }
+        // 각 코루틴 마지막 줄에서 Think 코루틴 다시 실행
         yield return null;
     }
 
     private IEnumerator NormalAttackCoroutine()
     {
-        yield return null;
-        StartCoroutine(WalkCoroutine());
+        for(int i = 0; i < 10; ++i)
+        {
+            isMove = false;
+            NormalAttack(); // Attack anim은 하나. Attack 함수는 4개? 어떻게 합치지?
+            yield return new WaitForSeconds(2.0f);
+            isMove = true;
+        }
+    }
+    private void NormalAttack()
+    {
+        // TODO : 랜덤한 라인에 몬스터 생성
     }
 
-    private IEnumerator FirstAttackCoroutine()
+    private IEnumerator FirstPatternCoroutine()
     {
         yield return null;
+        StartCoroutine(Think());
     }
 
-    private IEnumerator SecondAttackCoroutine()
+    private IEnumerator SecondPatternCoroutine()
     {
         yield return null;
+        StartCoroutine(Think());
     }
 
-    private IEnumerator ThirdAttackCoroutine()
+    private IEnumerator ThirdPatternCoroutine()
     {
         yield return null;
+        StartCoroutine(Think());
     }
 }
