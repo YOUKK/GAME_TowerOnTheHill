7156eb1d2d8874e888fa483262d0187af600ce1b
Dong Kyu Lee <stvboy00@gmail.com>
2023-11-28 16:24:34 +0900

    (WIP)Implementing Boss pattern

--- Assets/Scripts/Monster/BossMonster.cs
+++ Assets/Scripts/Monster/BossMonster.cs
@@ -12,39 +12,84 @@ public class BossMonster : Monster
     private GameObject thirdAttackObj;
     private List<MonsterSpawnData> monsterSpawns;
 
+    [SerializeField]
     private float patternDuration;
+    // Normal 공격을 할 차례인지를 나타내는 변수
+    private bool isNormalAttackTime;
     private bool isMove;
+    private bool firstHurt = false;
+    private bool secondHurt = false;
+    private float moveRange = 3.0f;
+    private Vector3 startPosition;
+    private int moveKey = 1;
 
     protected override void Start()
     {
         base.Start();
         pattern = AttackPattern.Normal;
+        isNormalAttackTime = true;
         patternDuration = 5.0f;
         isMove = false;
+        transform.position = new Vector3(6.5f, 1, -1);
+        startPosition = transform.position;
         StartCoroutine(Think());
     }
 
     protected override void Update()
     {
-        if(isMove)
+        if (isMove)
         {
             Move(currentSpeed);
         }
-    }
 
+        if (currentHP < (float)status.hp * 2 / 3 && firstHurt == false)
+        {
+            anim.SetTrigger("HurtTrigger");
+            firstHurt = true;
+        }
+        if (currentHP < (float)status.hp * 1 / 3 && secondHurt == false)
+        {
+            anim.SetTrigger("HurtTrigger");
+            secondHurt = true;
+        }
+    }
+    
     private void Idle()
     {
-        // Idle animation;
+        anim.SetBool("isPatternEnd", true);
+        isMove = false;
     }
 
     protected override void Move(float speed)
     {
-        // TODO : 위아래 이동으로 변경
-        base.Move(speed);
+        if (Mathf.Abs(transform.position.y - startPosition.y) < moveRange)
+        {
+            transform.position = new Vector3(transform.position.x,
+            transform.position.y + speed * moveKey * Time.deltaTime, transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x, 
+                startPosition.y + (moveRange - 0.01f) * moveKey, transform.position.z);
+            moveKey *= -1;
+        }
+    }
+
+    // 애니메이션 이벤트로 호출하는 함수
+    //Walk Animation에 의해 호출
+    private void MakeBossMove()
+    {
+        isMove = true;
     }
 
     private void ChangeAttackPattern()
     {
+        if (isNormalAttackTime)
+        {
+            pattern = AttackPattern.Normal;
+            return;
+        }
+        // 체력에 따라 실행할 직접 공격 패턴을 결정
         if(currentHP >= (float)status.hp * 2/3)
         {
             pattern = AttackPattern.First;
@@ -61,7 +106,11 @@ public class BossMonster : Monster
 
     private IEnumerator Think()
     {
+        Idle();
+        yield return new WaitForSeconds(patternDuration);
+        anim.SetTrigger("AttackPrepare");
         ChangeAttackPattern();
+        anim.SetBool("isPatternEnd", false);
         // switch문으로 상태 결정
         switch (pattern)
         {
@@ -125,46 +174,68 @@ public class BossMonster : Monster
     private void NormalAttack()
     {
         // TODO : 랜덤한 라인에 몬스터 생성 (한 공격 당 2마리씩 호출)
+        Debug.Log("Normal Attack");
     }
     private void FirstAttack()
     {
-
+        Debug.Log("First Attack");
     }
     private void SecondAttack()
     {
-
+        Debug.Log("Second Attack");
     }
     private void ThirdAttack()
     {
-
+        Debug.Log("Third Attack");
     }
 
     private IEnumerator NormalAttackCoroutine()
     {
-        
         for (int i = 0; i < 5; ++i)
         {
             isMove = false;
             anim.SetTrigger("AttackTrigger");
-            yield return new WaitForSeconds(1.0f);
-            isMove = true;
-            yield return new WaitForSeconds(3.0f);
+            yield return new WaitForSeconds(4.0f);
         }
+
+        isNormalAttackTime = false;
+        anim.SetBool("isPatternEnd", true);
         StartCoroutine(Think());
     }
     private IEnumerator FirstPatternCoroutine()
     {
-        yield return null;
+        isMove = false;
+        anim.SetTrigger("AttackTrigger");
+        yield return new WaitForSeconds(2.0f);
+
+        isNormalAttackTime = true;
+        anim.SetBool("isPatternEnd", true);
         StartCoroutine(Think());
     }
     private IEnumerator SecondPatternCoroutine()
     {
-        yield return null;
+        isMove = false;
+        pattern = AttackPattern.First;
+        anim.SetTrigger("AttackTrigger");
+        yield return new WaitForSeconds(patternDuration);
+        pattern = AttackPattern.Second;
+        anim.SetTrigger("AttackTrigger");
+
+        isNormalAttackTime = true;
+        anim.SetBool("isPatternEnd", true);
         StartCoroutine(Think());
     }
     private IEnumerator ThirdPatternCoroutine()
     {
-        yield return null;
+        isMove = false;
+        pattern = AttackPattern.Second;
+        anim.SetTrigger("AttackTrigger");
+        yield return new WaitForSeconds(patternDuration);
+        pattern = AttackPattern.Third;
+        anim.SetTrigger("AttackTrigger");
+
+        isNormalAttackTime = true;
+        anim.SetBool("isPatternEnd", true);
         StartCoroutine(Think());
     }
 }
