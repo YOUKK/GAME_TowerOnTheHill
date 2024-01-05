aec4e75e223c020d3473bb8700088a7880bfacec
Dong Kyu Lee <stvboy00@gmail.com>
2024-01-05 16:10:55 +0900

    Runtime bug fix

--- Assets/Scripts/Monster/MonsterSpawner.cs
+++ Assets/Scripts/Monster/MonsterSpawner.cs
@@ -153,7 +153,10 @@ public class MonsterSpawner : MonoBehaviour
         {
             foreach (var item in monsterList[i])
             {
-                item.GetComponent<Monster>().ChangeStatus(20, 0.2f, 50);
+                if (item != null)
+                {
+                    item.GetComponent<Monster>().ChangeStatus(20, 0.2f, 50);
+                }
             }
         }
         Invoke("NerfMonsters", monsterBuffTime);
@@ -165,7 +168,10 @@ public class MonsterSpawner : MonoBehaviour
         {
             foreach (var item in monsterList[i])
             {
-                item.GetComponent<Monster>().ChangeStatus();
+                if (item != null)
+                {
+                    item.GetComponent<Monster>().ChangeStatus();
+                }
             }
         }
 
