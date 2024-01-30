49ef2fa5ba8f6d2cb5c1347678fdee6fbf9b1235
Dong Kyu Lee <stvboy00@gmail.com>
2024-01-30 15:01:56 +0900

    (WIP) 도감 구현 중. Character description 작성 완료

--- Assets/Scripts/UI/TitleScene/TitleUIManager.cs
+++ Assets/Scripts/UI/TitleScene/TitleUIManager.cs
@@ -6,17 +6,28 @@ using TMPro;
 
 public class TitleUIManager : MonoBehaviour
 {
-    [SerializeField] private TMP_Text coinText;
+    [Header("Buttons")]
     [SerializeField] private Button playButton;
     [SerializeField] private Button tutorialButton;
     [SerializeField] private Button shopButton;
     [SerializeField] private Button bookButton;
     [SerializeField] private Button closeBookButton;
+
+    [Header("Texts")]
+    [SerializeField] private TMP_Text coinText;
+
+    [Header("Book Popup")]
     [SerializeField] private GameObject bookPopup;
+    [SerializeField] private Sprite targetImage;
+    [SerializeField] private TMP_Text targetName;
+    [SerializeField] private TMP_Text targetDescription;
+
+    //private Dictionary<string, >
 
     void Start()
     {
         coinText.text = PlayerPrefs.GetInt("coin").ToString();
+        // 버튼에 리스너 함수 추가
         playButton.onClick.AddListener(() => GameManager.GetInstance.MoveScene("Title", "LevelSelectScene"));
         tutorialButton.onClick.AddListener(() => GameManager.GetInstance.MoveScene("Title", "TutorialScene"));
         shopButton.onClick.AddListener(() => GameManager.GetInstance.MoveScene("Title", "Shop"));
@@ -31,6 +42,12 @@ public class TitleUIManager : MonoBehaviour
             bookPopup.SetActive(flag);
             SoundManager.Instance.PlayEffect("Button1");
         }
+    }
+
+    // 클릭한 캐릭터/몬스터의 정보를 표시할 팝업 창 갱신
+    public void ChangeBookPopup(string targetName)
+    {
+
     }
 
     public void ExitGame()
